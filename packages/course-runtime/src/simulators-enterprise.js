55626701ea7ec11cedf2ed4e3570c60c8ed8e071
colafang <colafang233@gmail.com>
2026-07-23 14:09:11 +0800

    feat: register enterprise platform simulators

--- packages/course-runtime/src/simulators-enterprise.js
+++ packages/course-runtime/src/simulators-enterprise.js
@@ -1,5 +1,7 @@
 import { enterpriseFoundationSimulators } from './simulators-enterprise-foundations.js';
+import { enterprisePlatformSimulators } from './simulators-enterprise-platform.js';
 
 export const enterpriseSimulators = {
   ...enterpriseFoundationSimulators,
+  ...enterprisePlatformSimulators,
 };

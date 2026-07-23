c95cae2abe7e5fbd818eb12037b52eb2d7219fea
colafang <colafang233@gmail.com>
2026-07-23 14:22:55 +0800

    feat: register all enterprise service-desk simulators

--- packages/course-runtime/src/simulators-enterprise.js
+++ packages/course-runtime/src/simulators-enterprise.js
@@ -1,7 +1,11 @@
 import { enterpriseFoundationSimulators } from './simulators-enterprise-foundations.js';
 import { enterprisePlatformSimulators } from './simulators-enterprise-platform.js';
+import { enterpriseFlowSimulators } from './simulators-enterprise-flows.js';
+import { enterpriseGovernanceSimulators } from './simulators-enterprise-governance.js';
 
 export const enterpriseSimulators = {
   ...enterpriseFoundationSimulators,
   ...enterprisePlatformSimulators,
+  ...enterpriseFlowSimulators,
+  ...enterpriseGovernanceSimulators,
 };
